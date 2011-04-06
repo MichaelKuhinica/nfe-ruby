006ce7f323431dc79f60eb73d10443cf97f51efd
unknown <VITAO@.(none)>
2011-04-05 21:13:55 -0300

    digital certficate test - waiting reply savon group

--- lib/nfe/service/web_service.rb
+++ lib/nfe/service/web_service.rb
@@ -7,17 +7,24 @@ module NFe
 
     class WebService
 
-      attr_accessor :soap_params
+      attr_accessor :soap_params, :client
 
-      def initialize(state='sp')
+      def initialize(user, pass, certificate, state='sp')
         raise 'parametro de inicializacao de webservice invalido' unless state.respond_to? :to_sym
+        @certificate = certificate
+        @user = user
+        @pass = pass
+        self.soap_params = NFe::Config::Params::URLS::SOAP_URLS[state.to_sym]
+      end
 
-        @@soap_params = NFe::Config::Params::URLS::SOAP_URLS[state.to_sym]
-
-        @@client = Savon::Client.new do |wsdl, http|
+      def status_servico
+        client = Savon::Client.new do |wsdl, http, wsse|
+          wsse.credentials @user, @pass, @certificate
           wsdl.document = "#{soap_params[:base_url]}#{soap_params[:status_servico]}?wsdl"
+          http.auth.ssl.cert_file = @certificate
         end
-        p @@client.wsdl.soap_actions
+
+        p client.wsdl.soap_actions
       end
 
     end
--- spec/nfe-ruby/service/web_service_status_spec.rb
+++ spec/nfe-ruby/service/web_service_status_spec.rb
@@ -1,9 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
+certificate_dir = File.expand_path(File.dirname(__FILE__) + '../../../../lib/nfe/certificates/certificado_restaurante_marias.pem')
 
 describe "WebService" do
 
   it "should get the status from the webservice" do
-    test = NFe::Service::WebService.new
+    test = NFe::Service::WebService.new('WGATI', "wgf39400", certificate_dir,'sp')
+    p test.status_servico
   end
 
 end
