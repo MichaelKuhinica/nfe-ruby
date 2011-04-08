c4a3887055c94d45a0a353b027aa36a9fb1058b1
vinicius gati <viniciusgati@gmail.com>
2011-04-07 23:00:32 -0300

    rebase3

--- lib/nfe/service/web_service.rb
+++ lib/nfe/service/web_service.rb
@@ -9,15 +9,32 @@ module NFe
 
       attr_accessor :soap_params
 
-      def initialize(state='sp')
-        raise 'parametro de inicializacao de webservice invalido' unless state.respond_to? :to_sym
+      def initialize(user, pass, certificate_path, state='sp')
+        raise 'estado em formato inválido' unless state.respond_to? :to_sym
+        @soap_params = NFe::Config::Params::URLS::SOAP_URLS[state.to_sym]
 
-        @@soap_params = NFe::Config::Params::URLS::SOAP_URLS[state.to_sym]
+        @url = "#{@soap_params[:base_url]}#{:status_servico}"
+        @certificate_path = certificate_path
+        @user = user
+        @pass = pass
+      end
 
-        @@client = Savon::Client.new do |wsdl, http|
-          wsdl.document = "#{soap_params[:base_url]}#{soap_params[:status_servico]}?wsdl"
-        end
-        p @@client.wsdl.soap_actions
+      def status_servico
+        uri = URI.parse(@url)
+        pem = File.read(@certificate_path)
+
+        http = Net::HTTP.new(uri.host, uri.port)
+
+        http.use_ssl = true
+        http.cert = OpenSSL::X509::Certificate.new(pem)
+
+        http.key = OpenSSL::PKey::RSA.new(pem)
+
+        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+        request = Net::HTTP::Get.new(uri.request_uri)
+
+        p request.body
       end
 
     end
