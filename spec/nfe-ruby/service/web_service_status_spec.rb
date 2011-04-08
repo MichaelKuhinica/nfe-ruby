8bbfc9ff0e3849ccc465dda5a216c3359744331b
vinicius gati <viniciusgati@gmail.com>
2011-04-07 22:59:45 -0300

    rebase3

--- spec/nfe-ruby/service/web_service_status_spec.rb
+++ spec/nfe-ruby/service/web_service_status_spec.rb
@@ -3,7 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
 describe "WebService" do
 
   it "should get the status from the webservice" do
-    test = NFe::Service::WebService.new
+    test = NFe::Service::WebService.new('', "", certificate_dir, 'sp')
+    p test.status_servico
   end
 
 end
