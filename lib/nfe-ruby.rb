2cfb8e8874ce3a753bcaf753441fb30e37d72d80
viniciusgati <viniciusgati@gmail.com>
2011-04-25 10:17:33 -0300

    needs reorganization, and fix requires

--- lib/nfe-ruby.rb
+++ lib/nfe-ruby.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'nfe/config/params'
-require 'nfe/entidades/nota_fiscal'
+require 'nfe/entidades/infNfe/nota_fiscal'
 
 if __FILE__==$0
   puts 'Rodando nfe'
